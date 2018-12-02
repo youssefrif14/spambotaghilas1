186d54576790d414b13814421f100311a13c0ed6
youssefrif14 <45339437+youssefrif14@users.noreply.github.com>
2018-12-02 14:12:18 +0300

    Update wes.js

--- wes.js
+++ wes.js
@@ -7,8 +7,8 @@ console.log(`Logged in as ${client.user.tag}!`);
 
 
 client.on('ready', async() => {
-var server = "512556758833823764";//ايدي السيرفر
-var channel = "512556850558795777";//ايدي الروم
+var server = "516318670016741397";//ايدي السيرفر
+var channel = "516318840666324992";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا يا خرا شنو هذا ')
 	},100);
@@ -22,4 +22,4 @@ client.on('message', message => {
    } 
 });
 
-client.login("توكن");
+client.login("NTE4NzQ1NDE5ODAzNzg3MjY0.DuVO0w.WaBdY3XFOOzPt7eynQPd0dwqyUc");
